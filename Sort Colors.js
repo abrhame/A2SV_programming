a685de28e8feb087dea2d3b86a246a25b0d1942c
resturant <99506193+abrhame@users.noreply.github.com>
2022-10-06 09:03:54 -0500

    Create Sort Colors.js

---
+++ Sort Colors.js
@@ -0,0 +1,10 @@
+var sortColors = function(nums) {
+    let i;
+    for( i= 0; i < nums.length; i++){
+        for(let j = 0; j< nums.length-1-i; j++){
+            if(nums[j]>nums[j+1]){
+               [nums[j],nums[j+1]] = [nums[j+1],nums[j]]
+            }
+        }
+    }
+};
