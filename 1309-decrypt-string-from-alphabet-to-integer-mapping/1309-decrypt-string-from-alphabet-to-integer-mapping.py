9966a9b2039e8791fb237025855af6b47e4c69d4
resturant <99506193+abrhame@users.noreply.github.com>
2022-12-23 13:53:03 -0600

    Update 1309-decrypt-string-from-alphabet-to-integer-mapping.py

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,23 +1,5 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-         # given string formed with digits and #
-        #map s to english lowecase letters
-        #(a->i) => (1->9)
-        #(j->z)=> (10#->26#)
-        #create a dictionary 
-        #loop and put them as key value pairs
-        
-        dictionary = {}
-#         for digit in range(1,27):
-#             #generate alpahbets automatically as keys
-#             print(digit)
-#             # dictionary = dict.fromkeys(string.ascii_lowercase, digit)
-           
-
-            
-#         a =  {chr(i+96):i for i in range(1,27)}
-#         print(a)
-
         maps = {}
         word = ""
         for i in range(1,27):
