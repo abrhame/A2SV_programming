eb3f9dee8c4f4d06c0441744431b3c79dfec2124
resturant <99506193+abrhame@users.noreply.github.com>
2022-12-28 14:25:19 -0600

    Time: 60 ms (82.29%), Space: 13.9 MB (81.48%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -1,11 +1,13 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        ans = Counter()
-        for domain in cpdomains:
-            count, domain = domain.split()
-            count = int(count)
-            frags = domain.split('.')
-            for i in range(len(frags)):
-                ans[".".join(frags[i:])] += count
+            ans = Counter()
+            for domain in cpdomains:
+                count,subDomain = domain.split()
+                count = int(count)
+                sub = subDomain.split(".")
+                for i in range(len(sub)):
+                    ans[".".join(sub[i:])] += count
+                    
+            return ["{} {}".format(ct, dom) for dom, ct in ans.items()]
 
-        return ["{} {}".format(ct, dom) for dom, ct in ans.items()]
+                    
