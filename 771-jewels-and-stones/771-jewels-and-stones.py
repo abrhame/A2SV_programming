5911a348aee9a97e8bd6fb7cd5da841c84e46754
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-01 11:07:17 -0600

    Time: 37 ms (76.93%), Space: 13.8 MB (58.23%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,7 +1,11 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        count = 0
+        lookup = set()
+        total_jewels = 0
+        for jewel in jewels:
+            lookup.add(jewel)
+        
         for stone in stones:
-            if stone in jewels:
-                count+=1
-        return count
+            if stone in lookup:
+                total_jewels +=1
+        return total_jewels
