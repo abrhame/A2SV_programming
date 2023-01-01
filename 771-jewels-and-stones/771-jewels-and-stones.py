4793df795e5e64fdfb74c1ee7fed8ecce01000a1
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-01 10:59:11 -0600

    Time: 22 ms (99.42%), Space: 14 MB (11.50%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for stone in stones:
+            if stone in jewels:
+                count+=1
+        return count
