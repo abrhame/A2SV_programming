d80e2657e9badff6cf8ec50b5fdd1c77bf511539
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-04 09:56:27 -0600

    Time: 1846 ms (20.04%), Space: 19 MB (55.42%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,9 +1,21 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        transpose = list(zip(*grid))
+        # transpose = list(zip(*grid))
+        # count = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(transpose)):
+        #         if grid[i] == list(transpose[j]):
+        #             count +=1
+        # return count
+        trans = []
+        for i in range(len(grid)):
+            tmp = []
+            for j in range(len(grid)):
+                tmp.append(grid[j][i])
+            trans.append(tmp)
         count = 0
         for i in range(len(grid)):
-            for j in range(len(transpose)):
-                if grid[i] == list(transpose[j]):
+            for j in range(len(trans)):
+                if grid[i] == trans[j]:
                     count +=1
         return count
