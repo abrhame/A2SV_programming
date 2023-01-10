bdbf0b24213fa6cc40350d0cb69a776765001e73
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-10 10:47:16 -0600

    Create Equal Row and Column Pairs.py

---
+++ Equal Row and Column Pairs.py
@@ -0,0 +1,25 @@
+        # transpose = list(zip(*grid))
+        # count = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(transpose)):
+        #         if grid[i] == list(transpose[j]):
+        #             count +=1
+        # return count
+        # trans = []
+        # for i in range(len(grid)):
+        #     tmp = []
+        #     for j in range(len(grid)):
+        #         tmp.append(grid[j][i])
+        #     trans.append(tmp)
+        # count = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(trans)):
+        #         if grid[i] == trans[j]:
+        #             count +=1
+        # return count
+        
+        c=Counter(tuple(row) for row in grid)
+        ans=0
+        for col in zip(*grid):
+            ans+=c[col]
+        return ans
