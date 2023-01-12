312d09dfbe65d0a3fd7de1a74b2e70ff52279264
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-12 07:01:50 -0600

    Time: 899 ms (70.42%), Space: 33.5 MB (24.61%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+
+        '''
+          101
+          010
+        '''
+        # ans = []
+        # ans.append(pref[0])
+        # i = 0
+        # while len(ans) < len(pref)  and i < float('inf'):
+        #     if pref[len(ans) - 1] ^ i == pref[len(ans)]: 
+        #         ans.append(i)
+        #         i = -1
+        #     i+=1
+        # return ans    
+        
+        ans = []
+        recent = []
+        recent.append(pref[0])
+        ans.append(pref[0])
+        for i in range(1, len(pref)):
+            tmp = pref[i]^pref[i-1]
+            ans.append(tmp)
+        return ans
