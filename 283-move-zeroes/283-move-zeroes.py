e789fc5439eae5de662b00bb3d7c909ae02699a6
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-12 00:21:28 -0600

    Time: 166 ms (92.96%), Space: 15.6 MB (61.89%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,19 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # use two pointer iterate over the arr and swap the elements
-        # for i in range(len(nums)):
-        #     if i+1< len(nums) and nums[i] == 0:
-        #         if nums[i+1]!=0:
-        #             nums[i],nums[i+1] = nums[i+1],nums[i]
-        #         if i+2< len(nums) and nums[i+2] == 0:
-        #             nums[i],nums[i+2] = nums[i+2],nums[i]
-        #     else:
-        #         continue
-        # return nums
-       	
-        i = 0
-        for num in range(len(nums)):
-            if nums[num] != 0:
-                nums[i],nums[num] = nums[num],nums[i]
-                i+=1
+        p = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[p] = nums[i]
+                p = p+1
+        for i in range(p,len(nums)):
+            nums[i] = 0
+        return nums
+            
+       
