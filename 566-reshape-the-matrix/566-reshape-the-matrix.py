b2c04b9e939f1200776a9d9747096f80fe08a8f7
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-14 16:06:04 -0600

    Time: 99 ms (60.09%), Space: 14.6 MB (77.52%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        # if the product of r and c and the product of the orginal row and column is not equal then it is not valid to reshape the matrix
         if (len(mat) * len(mat[0])) != (r * c):
             return mat
         matrix = [[] for __ in range(r)]
@@ -7,7 +8,11 @@ class Solution:
         a = 0
         for i in range(len(mat)):
             for j in range(len(mat[0])):
+                
+                # a is the index of the row
                 matrix[a].append(mat[i][j])
+                
+                # if the length of the ath row of thw matrix is equal to the target column then move to the next row
                 if len(matrix[a]) == c:
                     a +=1
         return matrix
