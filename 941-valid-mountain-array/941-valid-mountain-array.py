0fd1fe98594f369bb02e296a3b61e0467fce245c
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-17 15:54:08 -0600

    Time: 221 ms (59.28%), Space: 15.5 MB (18.91%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        # if the length of the array is less than 3 it is not a valid Mountain array
+        if len(arr) < 3:
+            return False
+        
+        # intialize a flag variable to follow the mountain start to stricly decresing
+        flag = False
+        idx = 0
+        for i in range(len(arr)-1):
+            if arr[i] == arr[i+1]: 
+                return False
+            elif arr[i] > arr[i +1]:
+                flag = True # if the array start to decrease intialize the flag to True
+                idx = i+1   # index of the array that it starts to decreas strictly
+                break
+            elif i+1 == (len(arr) - 1):
+                return False
+        if flag == True:    # if the flag  is true, check for strictly decreasing
+            for i in range(idx,len(arr)-1):
+                if arr[i] <= arr[i + 1]:
+                    return False
+                elif idx == 1 and i+1 == (len(arr) - 1):
+                    return False
+        return True
+                
+        
