e270ab452798aa8e35f182367852cd06c3b34b1f
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-18 12:13:22 -0600

    Time: 640 ms (80.70%), Space: 26.6 MB (36.77%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        # sort the 
+        # sort the piles
         piles.sort()
         ans = 0
-        count = 0
+        count = 0       # the limit of piles I can take, which is piles/3
+        
+        # find the next largest pile I can take 
         for i in range(len(piles)-2,-1,-2):
             
             ans += piles[i]
             count+=1
-            if count == len(piles)/3:
+            
+            if count == len(piles)/3:   # if I take all the piles  I am allowed then break out of the loop
                 break
         return ans
         
