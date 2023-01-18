2e6e8ab792766740864bdbf0e972c882c5042efa
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-18 11:43:05 -0600

    Time: 37 ms (95.28%), Space: 13.7 MB (97.78%) - LeetHub

--- 969-pancake-sorting/969-pancake-sorting.py
+++ 969-pancake-sorting/969-pancake-sorting.py
@@ -3,9 +3,12 @@ class Solution:
         """ sort like bubble-sort
             sink the largest number to the bottom at each round
         """
+        # our output is going to be the index of flip performed
         ans = []
+        
+        # a function that performs flips in place
         def flip(arr,k):
-            ans.append(k+1)
+            ans.append(k+1)   # the index of the flip going to be performed
             i = 0
             while i < k:
                 arr[i], arr[k] = arr[k], arr[i]
@@ -15,15 +18,13 @@ class Solution:
         count = 0
         for i in range(len(A)-1,-1,-1):
             max_ = A.index(max(A[0:i+1]))
+            # check wether the given list is sorted ans also check for the maximum element is in its expected place. if its in the correct place don't perform the flips
             if max_ == i:
-                count += 1
                 continue
-            flip(A,max_)
-           
-            flip(A,i)
-        if count == len(A):
-            return []
-        else:
-            return ans
+            flip(A,max_)    # flip the largest element to the 0 index
+            flip(A,i)   # flip the largest elemt to the correct postion. i.e next to the previous largest element
+            
+        # if the number of the count is the same as length of the list then it is already sorted
+        return ans
             
             
