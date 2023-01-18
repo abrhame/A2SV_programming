5ba00c8e50e5dadf585625dee5d21bfb01cf2850
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-18 12:59:28 -0600

    Time: 39 ms (86.26%), Space: 13.9 MB (64.06%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
+        # comparator function, compares by changeing the numbers to string
         def compare(x, y):
             num1 = str(x)+str(y)
             num2 = str(y)+str(x)
@@ -8,8 +9,10 @@ class Solution:
             else:
                 return -1
 
-    # Calling
+        # sort nums using the comparator function
         nums.sort(key=functools.cmp_to_key(compare), reverse=True)
+        
+        # concatenate nums 
         ans = ""
         for num in nums:
             ans += str(num)
