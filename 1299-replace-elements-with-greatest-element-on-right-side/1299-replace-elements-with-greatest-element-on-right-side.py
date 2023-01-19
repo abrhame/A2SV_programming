905b3c8fbbe2d09fecb79b94961e0a68bfabbf5e
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-19 11:02:58 -0600

    Time: 1169 ms (38.73%), Space: 15.7 MB (28.43%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -3,11 +3,11 @@ class Solution:
         #iterate all over the elment and find the maximum value to the right of the current element and replace it with the maximum value
         max_= 0
         for i in range(len(arr)-1):
-            if max_ > 0 and ind > i:
+            if max_ > 0 and ind > i:   # i tried to optimize the time complexity by not finding the maximum element until the index of maximum elment is greater than the current index
                 arr[i] = max_
             else:
-                max_= max(arr[i+1:len(arr)])
-                ind = arr.index(max_)
+                max_= max(arr[i+1:len(arr)])    # find the index of the maximum elemnt to the right of the current elment
+                ind = arr.index(max_)   # find the index of the maximum elemnt
                 arr[i] = max_
         # make the last element -1
         arr.pop()
