64c2c040b7034c08c0451a9b8a09c2cc7b20d0bc
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-24 16:26:14 -0600

    Time: 2675 ms (6.76%), Space: 25.4 MB (28.69%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(k):
             p1 = 0
             p2 = len(nums) - 1
-            
+            # insert the last element to the zero index and remove it from the nums
             nums.insert(p1, nums[p2])
             nums.pop()
             
