a2335b58ec829005dcb14063be14fafbf11a174f
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-24 16:42:44 -0600

    Time: 88 ms (82.67%), Space: 15.6 MB (56.67%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -1,9 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        p1 = 0
-        p2 = 1
+        p1 = 0     # the holder
+        p2 = 1     # the seeker
         
         while p2 < len(nums):
+            # find the elemnt that is diffrent from the the holder and move it next to the holder
             if nums[p1] != nums[p2]:
                 nums[p1+1] = nums[p2]
                 p1 += 1
