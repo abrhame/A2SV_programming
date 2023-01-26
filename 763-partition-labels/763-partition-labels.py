c93ba06225f0674c3cfb0c993b8fbb0b2eecd59b
resturant <99506193+abrhame@users.noreply.github.com>
2023-01-26 11:05:00 -0600

    Time: 49 ms (48.14%), Space: 13.8 MB (61.16%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -20,26 +20,5 @@ class Solution:
     
             
               
-#         for i in range(len(s)):
-#         ans = []
-#         ind = 0
-#         for i in range(len(s)):
-#             if s[i] in s[i+1:]:
-#                 ind = s[i+1:].index(s[i])
-#                 seeker = max(ind,seeker)
-#             else:
-#                 ans.append(seeker-holder)
-#                 holder = seeker
-#         return ans
-                
-        
-        
-#         while seeker < len(s):
-#             if (s[seeker] in s[holder:seeker]) and (s[seeker] not in s[seeker+1:len(s)]):
-#                 ans.append(seeker-holder)
-#                 holder = seeker + 1
-#                 seeker = holder
-#             else:
-#                 seeker += 1
-#         return ans
+
                 
