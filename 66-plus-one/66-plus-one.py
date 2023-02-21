18e6a82ae329bf241cb2fc796c238db011546ce6
resturant <99506193+abrhame@users.noreply.github.com>
2023-02-21 06:31:02 -0600

    Time: 36 ms (53.81%), Space: 13.9 MB (49.32%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,12 +1,26 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        ans = ""
-        for num in digits:
-            ans += str(num)
-        ans = int(ans)+1
-        ans = list(str(ans))
-        res = []
-        for num in ans:
-            res.append(int(num))
+        if digits[-1] == 9:
+            if len(digits) == 1:
+                return [1,0]
+            return self.plusOne(digits[:-1]) + [0]
+        else:
+            digits[-1] += 1
+        return digits
+                
+
             
-        return res
+            
+            
+            
+            
+#         ans = ""
+#         for num in digits:
+#             ans += str(num)
+#         ans = int(ans)+1
+#         ans = list(str(ans))
+#         res = []
+#         for num in ans:
+#             res.append(int(num))
+            
+#         return res
