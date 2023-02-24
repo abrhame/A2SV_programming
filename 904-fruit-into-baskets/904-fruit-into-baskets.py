98374dae49ca570de28e4ee37e0e045f62d12527
resturant <99506193+abrhame@users.noreply.github.com>
2023-02-24 13:36:48 -0600

    Time: 1130 ms (16.15%), Space: 20.2 MB (53.04%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -5,12 +5,14 @@ class Solution:
         l = 0
         for i in range(len(fruits)):
             basket[fruits[i]] += 1
-            if len(basket) > 2:
-                res = max(res, basket.total()-basket[fruits[i]])
+            while len(basket) > 2:
                 basket[fruits[l]] -= 1
+            
                 if basket[fruits[l]] == 0:
                     del basket[fruits[l]]
+                    
                 l += 1
+                
 
-        res = max(res, basket.total())
+            res = max(res, i - l + 1)
         return res
