496ae8f94683312185124de10499cd5314025afa
resturant <99506193+abrhame@users.noreply.github.com>
2023-02-26 02:06:56 -0600

    Time: 306 ms (73.78%), Space: 16.5 MB (98.12%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        curSum = 0
+        prefixSums = { 0 : 1}
+        
+        for n in nums:
+            curSum += n
+            diff = curSum - k
+            
+            res += prefixSums.get(diff,0)
+            prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+        return res
+        
+            
