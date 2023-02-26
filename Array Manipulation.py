6bd01b1f41cf55459a87cace92a2be21a14c1a64
resturant <99506193+abrhame@users.noreply.github.com>
2023-02-26 14:26:49 -0600

    Create Array Manipulation.py

---
+++ Array Manipulation.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    arr = [0] * (n+1)
+    # add the value at first index
+    # subtract the value at last index + 1
+    for query in queries:
+        start, end, k = query
+        arr[start - 1] += k
+        arr[end] -= k
+    
+    max_ = -1
+    running = 0
+    for k in arr:
+        running += k
+        if running > max_:
+            max_ = running
+    return max_
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
