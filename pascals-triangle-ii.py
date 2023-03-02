c9c6c35b41d82c55725058957fccf82f102a370f
resturant <99506193+abrhame@users.noreply.github.com>
2023-03-02 13:13:19 -0600

    added pascals-triangle-ii.python

--- pascals-triangle-ii.py
+++ pascals-triangle-ii.py
@@ -1,14 +1,30 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        n = rowIndex
-        res = [[1]]
-        for i in range(n):
-            temp = [1] * (2 + i)
-            if i > 0:
-                for j in range(i):
-                    temp[j+1] = sum(res[-1][j:j+2])
-                    print(res[-1])
-                    print(temp)
-            res.append(temp)
-        print(res)
-        return res[-1]
+        
+        def pascal(row,n):
+            if n == rowIndex:
+                return row 
+            temp = [1] * (2 + n)
+
+            if len(row) >=2:
+                for j in range(len(row)):
+                    temp[j+1] = sum(row[j:j+2])
+
+            return pascal(temp,n+1)
+        return pascal([1],0)
+        
+
+            
+
+
+
+
+        # n = rowIndex
+        # res = [[1]]
+        # for i in range(n):
+        #     temp = [1] * (2 + i)
+        #     if i > 0:
+        #         for j in range(i):
+        #             temp[j+1] = sum(res[-1][j:j+2])
+        #     res.append(temp)
+        # return res[-1]
