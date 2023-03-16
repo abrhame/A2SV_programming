45155f0029be530b163837b337501c66a76dc4dd
resturant <99506193+abrhame@users.noreply.github.com>
2023-03-16 01:49:25 -0500

    added car-fleet.python

---
+++ car-fleet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p,s] for p,s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p,s in pair:
+            stack.append((target-p)/s)
+            while len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+            
+        return len(stack)
