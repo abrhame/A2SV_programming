74f6f6a1bb927be69404802408ca66c883bc31fb
resturant <99506193+abrhame@users.noreply.github.com>
2023-03-20 00:21:01 -0500

    added remove-duplicate-letters.python

---
+++ remove-duplicate-letters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        n = len(s)
+        stack = []
+        count = Counter(s)
+        seen = set()
+        for ch in s:
+            if ch not in seen:
+                while stack and stack[-1] >= ch and count[stack[-1]] > 1:
+                    c = stack.pop()
+                    count[c] -= 1
+                    seen.remove(c)
+                stack.append(ch)
+                seen.add(ch)
+            else:
+                count[ch] -= 1
+        return "".join(stack)
