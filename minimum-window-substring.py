e921b68dff554e110d502c43dd4333c6fad9a4b8
resturant <99506193+abrhame@users.noreply.github.com>
2023-03-22 04:18:07 -0500

    added minimum-window-substring.python

---
+++ minimum-window-substring.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == '': return ''
+                
+        t_dict, window = Counter(t), Counter()
+        
+                    
+        have, need = 0, len(t_dict)
+        res, resLen = [-1, -1], float('inf')
+        l = 0
+                
+        for r in range(len(s)):
+            c = s[r]
+            window[c] += 1
+                    
+            if window[c] == t_dict[c]:
+                have += 1
+                        
+            while have == need:
+                # update our result
+                if r - l + 1 < resLen:
+                    resLen = r - l + 1
+                    res = [l, r]
+                            
+                # pop from left of window
+                window[s[l]] -= 1
+                if s[l] in t_dict and window[s[l]] < t_dict[s[l]]:
+                    have -= 1
+                            
+                l += 1
+                        
+        l, r = res
+        return s[l:r+1]
