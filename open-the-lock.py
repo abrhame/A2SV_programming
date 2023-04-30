bd5d89db808e09d4d933197819dbbf39d0eabb7c
Abrham Lakew <99506193+abrhame@users.noreply.github.com>
2023-04-30 02:45:39 -0500

    added open-the-lock.python

--- open-the-lock.py
+++ open-the-lock.py
@@ -1,28 +1,32 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         queue = deque()
-        queue.append(("0000",0))
+        queue.append("0000")
         deadends = set(deadends)
         visited = set()
         visited.add("0000")
-
+        turn = -1
         while queue:
-            string, turn = queue.popleft()
+            n = len(queue)
+            turn += 1
 
-            if string == target:
-                return turn
-            
-            if string in deadends:
-                continue
+            for _ in range(n):
+                string = queue.popleft()
 
-            for i in range(4):
-                digit = int(string[i])
-                for dire in [-1,1]:
-                    new_digit = (digit + dire) % 10
-
-                    n_string = string[:i] + str(new_digit) + string[i+1:]
-                    if n_string not in visited:
-                        visited.add(n_string)
-                        queue.append((n_string, turn + 1))
+                if string == target:
+                    return turn
+                
+                if string in deadends:
+                    continue
+        
+                for i in range(4):
+                    digit = int(string[i])
+                    for dire in [-1,1]:
+                        new_digit = (digit + dire) % 10
+                        print(string)
+                        n_string = string[:i] + str(new_digit) + string[i+1:]
+                        if n_string not in visited:
+                            visited.add(n_string)
+                            queue.append(n_string)
             
         return -1
