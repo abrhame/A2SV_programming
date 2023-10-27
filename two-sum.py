88a025ab7c6c4066986a17f235616b2322786ec4
Abrham Lakew <99506193+abrhame@users.noreply.github.com>
2023-10-27 19:22:30 +0300

    added two-sum.python

---
+++ two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in visited:
+                return [visited[target - nums[i]] , i]
+
+            else:
+                visited[nums[i]] = i
